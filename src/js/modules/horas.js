07401a2b78857e03803587d4607a2ad74711072a
Cesar <jcchoque23@gmail.com>
2026-09-04 02:52:23 -0400

    739

--- src/js/modules/horas.js
+++ src/js/modules/horas.js
@@ -18,6 +18,11 @@
     function terminoBusqueda(e) {
       busqueda[e.target.name] = e.target.value;
 
+      //Reiniciar los campos ocultos y el selector de horas
+      inputHiddenHora.value = '';
+      inputHiddenDia.value = '';
+      const horaPrevia = document.querySelector('.horas__hora--seleccionada');
+
       if (Object.values(busqueda).includes('')) {
         return;
       }
@@ -35,10 +40,14 @@
       obtenerHorasDisponibles(eventos);
     }
     function obtenerHorasDisponibles(eventos) {
+      //Reiniciar las Horas
+      const listadoHoras = document.querySelectorAll('#horas li');
+
+      listadoHoras.forEach((li) => li.classList.add('horas__hora--deshabilitada'));
+
       //Comprobar eventos ya tomados, y quitar la variable de deshabilitado
       const horasTomadas = eventos.map((evento) => evento.hora_id);
 
-      const listadoHoras = document.querySelectorAll('#horas li');
       const listadoHorasArray = Array.from(listadoHoras);
 
       const resultado = listadoHorasArray.filter((li) => !horasTomadas.includes(li.dataset.horaId));
@@ -61,6 +70,9 @@
       e.target.classList.add('horas__hora--seleccionada');
 
       inputHiddenHora.value = e.target.dataset.horaId;
+
+      //Llenar el campo oculto de día
+      inputHiddenDia.value = document.querySelector('[name="dia"]:checked').value;
     }
   }
 })();
